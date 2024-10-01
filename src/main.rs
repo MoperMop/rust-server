9f4203b14cbc96b39afce6d5994b9cc078bf29bf
daniel <dantiptoe@gmail.com>
2024-10-01 13:48:50 -0600

    read HTTP requests

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
-
-        println!("Connection established!");
+        handle_connection(stream.unwrap());
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let http_request: Vec<_> = BufReader::new(&mut stream)
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}\n");
 }
