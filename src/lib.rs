521e72a13063631a11cc7a14910083c2299f7d8b
daniel <dantiptoe@gmail.com>
2024-10-02 14:15:47 -0600

    added Worker struct

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,36 @@ use std::thread;
 
 
 /// A pool of threads for handling tasks.
-pub struct ThreadPool;
+pub struct ThreadPool {
+    _workers: Vec<Worker>,
+}
 impl ThreadPool {
     /// Create a new ThreadPool.
     ///
     /// Size is the number of threads in the pool.
-    pub fn new(_size: usize) -> ThreadPool {
-        ThreadPool
+    pub fn new(size: usize) -> ThreadPool {
+        let mut _workers = Vec::with_capacity(size);
+
+
+        for _ in 0..size {
+            _workers.push(Worker::new());
+        }
+
+
+        ThreadPool { _workers }
     }
 
     /// Add the given callback to the queue.
     pub fn execute(&self, f: impl FnOnce() + Send + 'static) {
         thread::spawn(f);
+    }
+}
+
+struct Worker{
+    thread: thread::JoinHandle<()>,
+};
+impl Worker {
+    fn new() -> Worker {
+        Worker(thread::spawn(|| {}))
     }
 }
