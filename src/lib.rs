2250f66e5c686be9d86d5dfc02d617abb98264ad
daniel <dantiptoe@gmail.com>
2024-10-02 22:02:39 -0600

    added code for graceful shutdown

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,8 @@ use std::{thread, sync::{mpsc, Arc, Mutex}};
 
 /// A pool of threads for handling a task.
 pub struct ThreadPool<T: Send + 'static> {
-    _threads: Vec<thread::JoinHandle<()>>,
-    sender: mpsc::Sender<T>,
+    threads: Vec<Option<thread::JoinHandle<()>>>,
+    sender: Option<mpsc::Sender<T>>,
 }
 impl<T: Send + 'static> ThreadPool<T> {
     /// Create a new ThreadPool which will handle the given callback.
@@ -24,20 +24,34 @@ impl<T: Send + 'static> ThreadPool<T> {
         for _ in 0..size {
             let reciever = Arc::clone(&reciever);
             let mut f = f.clone();
-            threads.push(thread::spawn(move || {
+            threads.push(Some(thread::spawn(move || {
                 loop {
-                    let stream = reciever.lock().unwrap().recv().unwrap();
-                    f(stream);
+                    match reciever.lock().unwrap().recv() {
+                        Ok(stream) => f(stream),
+                        Err(_) => break,
+                    }
                 }
-            }));
+            })));
         }
 
 
-        ThreadPool { _threads: threads, sender }
+        ThreadPool { threads, sender: Some(sender) }
     }
 
     /// Send the given data to the threads.
     pub fn send(&self, data: T) {
-        self.sender.send(data).unwrap();
+        self.sender.as_ref().unwrap().send(data).unwrap();
+    }
+}
+
+impl<T: Send + 'static> Drop for ThreadPool<T> {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for thread in &mut self.threads {
+            if let Some(thread) = thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
