4f8963d79ed9f38801a10d303f8d2885607ae0d5
daniel <dantiptoe@gmail.com>
2024-10-02 21:01:36 -0600

    finished implementing ThreadPool + simplified

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,43 @@
-use std::thread;
+use std::{thread, net::TcpStream, sync::{mpsc, Arc, Mutex}};
 
 
 /// A pool of threads for handling tasks.
 pub struct ThreadPool {
-    _workers: Vec<Worker>,
+    _threads: Vec<thread::JoinHandle<()>>,
+    sender: mpsc::Sender<TcpStream>,
 }
 impl ThreadPool {
-    /// Create a new ThreadPool.
+    /// Create a new ThreadPool which will handle the given callback.
     ///
     /// Size is the number of threads in the pool.
-    pub fn new(size: usize) -> ThreadPool {
-        let mut _workers = Vec::with_capacity(size);
+    pub fn new(
+        f: impl FnMut(TcpStream) + Send + 'static + Clone,
+        size: usize,
+    ) -> ThreadPool {
+        let mut threads = Vec::with_capacity(size);
+
+
+        let (sender, reciever) = mpsc::channel();
+        let reciever = Arc::new(Mutex::new(reciever));
 
 
         for _ in 0..size {
-            _workers.push(Worker::new());
+            let reciever = Arc::clone(&reciever);
+            let mut f = (f).clone();
+            threads.push(thread::spawn(move || {
+                loop {
+                    let stream = reciever.lock().unwrap().recv().unwrap();
+                    f(stream);
+                }
+            }));
         }
 
 
-        ThreadPool { _workers }
+        ThreadPool { _threads: threads, sender }
     }
 
     /// Add the given callback to the queue.
-    pub fn execute(&self, f: impl FnOnce() + Send + 'static) {
-        thread::spawn(f);
-    }
-}
-
-struct Worker{
-    thread: thread::JoinHandle<()>,
-}
-impl Worker {
-    fn new() -> Worker {
-        Worker { thread: thread::spawn(|| {}) }
+    pub fn execute(&self, stream: TcpStream) {
+        self.sender.send(stream).unwrap();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ use rust_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    let pool = ThreadPool::new(handle_connection, 4);
 
     for stream in listener.incoming() {
-        pool.execute(|| { handle_connection(stream.unwrap()); });
+        pool.execute(stream.unwrap());
     }
 }
 
